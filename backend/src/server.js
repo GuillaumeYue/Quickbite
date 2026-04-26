2e52d188d6f342913e16713e71b671930e13b0d5
GuillaumeYue <alexyuehan@gmail.com>
2026-04-26 02:06:40 +0000

    add socket io with http server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const menuRoutes = require('./routes/menuRoutes');
@@ -21,7 +23,21 @@ app.use('/api/auth', authRoutes);
 app.use('/api/menu', menuRoutes);
 app.use('/api/orders', orderRoutes);
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+app.set('io', io);
+
+io.on('connection', function(socket) {
+  console.log('socket connected: ' + socket.id);
+  socket.on('disconnect', function() {
+    console.log('socket disconnected: ' + socket.id);
+  });
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
