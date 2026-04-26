bf9911d3989fbf2cb4bac4518e39eb296648a2a6
GuillaumeYue <alexyuehan@gmail.com>
2026-04-26 01:06:17 +0000

    add bcrypt password hashing to user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -20,5 +21,18 @@ const userSchema = new mongoose.Schema({
     default: 'customer'
   }
 }, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(plain) {
+  return await bcrypt.compare(plain, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
